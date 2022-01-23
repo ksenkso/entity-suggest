46afdb6868f5833fa5be77822eab2841ab1a6d9d
ksenkso <yazunya@yandex.ru>
2022-01-24 00:20:04 +0300

    feat: add icons

---
+++ src/components/Icon.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg class="icon">
+    <use :href="`symbols.svg#${name}`" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
